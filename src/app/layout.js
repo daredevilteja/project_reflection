1fe0d3fbaaab67f4ca43f19e7e46a873f44cddfe
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-22 23:20:26 +0530

    Added basic layout structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex flex-col flex-1 w-screen h-screen">
+          <header className="flex flex-1 items-center px-6">
+            <h1>Reflection</h1>
+          </header>
+          <main className="flex flex-5">
+            <aside className="flex flex-1">
+              <nav></nav>
+            </aside>
+            <section className="flex flex-3">{children}</section>
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
