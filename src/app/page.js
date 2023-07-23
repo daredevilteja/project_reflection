eec551c75df5bf3316173e085098cee9d603325f
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-23 10:24:42 +0000

    feat(app): add reflection form with select input and button

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return <main></main>;
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+  return (
+    <main>
+      <h1>Add a reflection</h1>
+      <select value={selectedOption} onChange={handleOptionChange}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+
+      <button>Add Reflection</button>
+    </main>
+  );
 }
