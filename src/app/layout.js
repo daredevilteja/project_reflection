ceff26665054ef1c50b27995292b2fc4bb542ae1
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-23 07:04:18 +0000

    refactor(layout): replace main tag with section tag in layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,12 +16,12 @@ export default function RootLayout({ children }) {
           <header className="flex flex-1 items-center px-6 border-b border-black border-solid">
             <h1>Reflection</h1>
           </header>
-          <main className="flex flex-5">
+          <section className="flex flex-5">
             <aside className="flex flex-1 border-r border-black border-solid">
               <nav></nav>
             </aside>
             <section className="flex flex-3">{children}</section>
-          </main>
+          </section>
         </div>
       </body>
     </html>
