f6bea818f8a05dfee2df0b67cf50b235f8e4586a
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-25 22:09:17 +0530

    Revert "Draft/sad forest"

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,17 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+async function getData() {
+  const reflections = await prisma.reflectionList.findMany();
 
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
 
 export default async function Home() {
   const displayReflections = async () => {
