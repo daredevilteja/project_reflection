7dada322f7af95810630d727bff945faf2e5e85a
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-25 17:52:59 +0000

    refactor(page): refactor getData function and add orderBy to findMany

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,14 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function getData() {
-  const reflections = await prisma.reflectionList.findMany();
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
 export default async function Home() {
   const displayReflections = async () => {
     "use server";
-    const items = await prisma.reflectionList.findMany();
+    const items = await prisma.reflectionList.findMany({
+      orderBy: {
+        id: "desc",
+      },
+    });
     return items;
   };
   const createReflection = async (val) => {
