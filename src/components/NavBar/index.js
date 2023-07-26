3bed2cddd590bb531c488de8ecb87689c604e614
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-26 12:56:22 +0000

    style(NavBar): Add motion animations and transitions to navigation items

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,33 +2,49 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function NavBar() {
   const pathname = usePathname();
   return (
     <nav>
       <ul className="flex flex-col gap-3 justify-center text-center px-3 py-2">
-        <li
+        <motion.li
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          whileHover={{ scale: 1.1 }}
+          whileFocus={{ scale: 1.1 }}
           className={`border border-gray-600 px-2 py-1 rounded w-[100%] ${
             pathname === "/" ? "active" : ""
           }`}
         >
           <Link href="/">Home</Link>
-        </li>
-        <li
+        </motion.li>
+        <motion.li
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          whileHover={{ scale: 1.1 }}
+          whileFocus={{ scale: 1.1 }}
           className={`border border-gray-600 px-2 py-1 rounded w-[100%] ${
             pathname === "/statistics" ? "active" : ""
           }`}
         >
           <Link href="/statistics">Statistics</Link>
-        </li>
-        <li
+        </motion.li>
+        <motion.li
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          whileHover={{ scale: 1.1 }}
+          whileFocus={{ scale: 1.1 }}
           className={`border border-gray-600 px-2 py-1 rounded w-[100%] ${
             pathname === "/contact" ? "active" : ""
           }`}
         >
           <Link href="/contact">Contact</Link>
-        </li>
+        </motion.li>
       </ul>
     </nav>
   );
